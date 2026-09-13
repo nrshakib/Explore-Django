fd0af5937c6c4da15ac4ebd7e1e37ee90a4948ac
nrshakib <nrshakib007@gmail.com>
2026-09-13 19:17:57 +0600

    Added Steps

---
+++ topic-exploration/steps.py
@@ -0,0 +1,20 @@
+# i) Install Python
+
+# ii) Create Virtual Environment using cmd: python -m venv environmentName
+
+# iii) Activate Venv using .\environmentName\Scripts\Activate.ps1
+
+# iv) Install Django inside venv using: python -m pip install Django
+
+# v) Check Django version: python -m django --version
+
+# vi) Create Project: python -m django startproject ProjectName
+
+# vii) change directory to newly created project and run server:
+
+#             ->  cd myproject [ command to change directory ]
+#             -> python manage.py runserver  [ command to run server ]
+
+# Ctrl + c to stop the server
+
+# enter deactivate command to close venv
