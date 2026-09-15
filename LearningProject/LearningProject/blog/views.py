517596f211b921c956eaf0d6237f09f37be985a8
nrshakib <nrshakib007@gmail.com>
2026-09-15 14:36:16 +0600

    Url Parameters

--- LearningProject/LearningProject/blog/views.py
+++ LearningProject/LearningProject/blog/views.py
@@ -5,3 +5,15 @@ def home(request):
 
 def about(request):
     return HttpResponse('Welcome to Blog About')
+
+def blog_details(request, blog_id):
+    return HttpResponse(f'Blog details {blog_id}')
+
+def user_profile(request, user_name):
+    return HttpResponse(f'User name: {user_name}')
+
+def blog_by_year(request, year):
+    return HttpResponse(f'Blog of {year}')
+
+def blog_by_year_month(request, **kwargs):
+    return HttpResponse(f'This blog is of Month: {kwargs['month']}, Year: {kwargs['year']}')
