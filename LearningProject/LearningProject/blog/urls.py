7812ba1acc1b8acc123e4c173f6a2a96f5a1be1a
nrshakib <nrshakib007@gmail.com>
2026-09-15 14:36:16 +0600

    Url Parameters

--- LearningProject/LearningProject/blog/urls.py
+++ LearningProject/LearningProject/blog/urls.py
@@ -1,7 +1,23 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.home, name = 'Blog Home'),
-    path('about/', views.about, name = 'Blog About')
+    path('about/', views.about, name = 'Blog About'),
+
+    # url parameters
+
+    # path
+    path('<int:blog_id>/', views.blog_details, name = 'Blog Details'),
+    path('user/<str:user_name>/', views.user_profile, name = 'User Profile'),
+
+    # multiple params
+    path('year/<int:year>/<int:month>', views.blog_by_year_month, name = 'Blog By Year Month'),
+
+    # regex_path
+    re_path(r'^year/(?P<year>[0-9]{4})/$',
+            views.blog_by_year,
+            name='Blog by year')
+
+    
 ]
