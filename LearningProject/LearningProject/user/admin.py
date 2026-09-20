01df35fa3f4565473c4a24aea574ca00f332c7e7
nrshakib <nrshakib007@gmail.com>
2026-09-20 14:28:59 +0600

    Manage models using Django Admin

--- LearningProject/LearningProject/user/admin.py
+++ LearningProject/LearningProject/user/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from user.models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display= ('name', 'age', 'city')
+    search_fields = ('name', 'city')
+    list_filter = ('name', 'age', 'city')
+    ordering = ('name',)
+
+
--- LearningProject/LearningProject/user/models.py
+++ LearningProject/LearningProject/user/models.py
@@ -4,3 +4,4 @@ class User(models.Model):
     name = models.CharField(max_length=50)
     age = models.IntegerField()
     email = models.EmailField(unique=True)
+    city = models.CharField(max_length=100)
